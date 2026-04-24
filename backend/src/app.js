0c8424871b79371f17ff043b3b19f733d9f09c79
Riya-DSilva <riya.dsilva@rthetapi.com>
2026-04-24 15:55:13 +0530

    Final

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,13 @@ import routes from "./routes/index.js";
 
 const app = express();
 
-app.use(cors({ origin: env.clientUrl, credentials: true }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://bhojan-setu-kappa.vercel.app/"
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: "12mb" }));
 app.use(express.urlencoded({ extended: true, limit: "12mb" }));
 app.use(requestLogger);
